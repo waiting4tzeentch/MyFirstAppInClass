dd206c3f9257bae178e5b20bb9fea1235e7e898a
Moser <michael.opper.moser@gmail.com>
2015-09-14 19:43:40 -0400

    Initial commit
    work, damn you!

---
+++ app/src/main/java/com/flapdoodle/michaelmoser/myfirstappinclass/StartupActivity.java
@@ -0,0 +1,56 @@
+package com.flapdoodle.michaelmoser.myfirstappinclass;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class StartupActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+    }
+
+    /**
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_startup, menu);
+        return true;
+    }
+     **/
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // sends a message to a new screen for display
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.text_input);
+
+        // pass text through editText to the EXTRA_MESSAGE constant
+        intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
+
+        // launch DisplayMessageActivity
+        startActivity(intent);
+    }
+}
